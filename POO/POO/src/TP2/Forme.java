82f5fafc7c3fe86a6d9bf57c40b8b8d11872e838
HugoDesb <hugo.desbiolles@gmail.com>
2017-09-28 12:02:30 +0200

    TP2 POO

---
+++ POO/POO/src/TP2/Forme.java
@@ -0,0 +1,9 @@
+package TP2;
+
+public abstract class Forme {
+
+	
+	public abstract void dessiner(PileTransformations pt);
+	public abstract void deplacer(Point p);
+		
+}
---
+++ POO/POO/src/TP2/Point.java
@@ -0,0 +1,62 @@
+package TP2;
+
+/**
+ * @author d16023863
+ *	Classe représentant un Point
+ */
+public class Point {
+	
+	private int x,y;
+	
+	/**
+	 * Constructeur par défaut
+	 */
+	public Point(){
+		this.x = 0; 
+		this.y = 0;
+	}
+	
+	/**
+	 * Constructeur
+	 * @param x
+	 * @param y
+	 */
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	/**
+	 * Ajoute le Point p avec le point de référence.
+	 * @param p
+	 * @return un nouveau point
+	 */
+	public Point add(Point p){
+		return new Point(this.x+p.getX(),this.y+p.getY());
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + x + ", " + y + ")";
+	}
+
+	
+	
+	
+}
